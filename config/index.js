667c1141f7fbac58df40aac0ec599366574534fe
rkrealizt <rk.realizt@gmail.com>
2024-10-02 22:23:00 +1300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-let uptimeRobotToken = "ur899712-6058a7b7cacc84540a172c95";
+let uptimeRobotToken = "ur2694038-68789ff9755ddddf08db5749";
 // let uptimeRobotToken = "ur899712-6058a7b7cacc84540a172c5";
 
 export { uptimeRobotToken };
